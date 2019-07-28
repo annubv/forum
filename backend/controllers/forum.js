c5fcc866027ccaa5fdaa1964e4beae4a21dac99a
annubv <annubv25@gmail.com>
2019-07-28 07:30:28 +0530

    added forum specifc page and css mods

--- backend/controllers/forum.js
+++ backend/controllers/forum.js
@@ -1,5 +1,6 @@
 const user = require("../database/models/user");
 const posts = require("../database/models/posts");
+const comments = require("../database/models/comments");
 
 const addforumpage = (req, res) => {
   const name = req.session.user.name;
@@ -8,9 +9,12 @@ const addforumpage = (req, res) => {
 
 const addforum = (req, res) => {
   const { data } = req.body;
-  const id = req.session.user.id;
-  console.log({ data, id });
-  var posts_instance = new posts({ user: id, data });
+  var posts_instance = new posts({
+    user_id: req.session.user.id,
+    user_name: req.session.user.name,
+    user_email: req.session.user.email,
+    data
+  });
   posts_instance.save(err => {
     if (err) {
       console.log("Error occured in adding forum : " + err);
@@ -19,6 +23,39 @@ const addforum = (req, res) => {
       return res.redirect("/");
     }
   });
+};
+
+const forumdata = (req, res) => {
+  const { pid } = req.body;
+  posts
+    .findOne()
+    .where(_id)
+    .equals(pid)
+    .populate("posts")
+    .exec((err, postsresult) => {
+      if (err) {
+        console.log("Error occured in fetching post: " + err);
+      } else {
+        comments
+          .find()
+          .where(post_id)
+          .equals(pid)
+          .populate("comments")
+          .exec((err, commentsresult) => {
+            if (err) {
+              console.log("Error occured in fetching comments: " + err);
+            } else {
+              console.log("Comments: " + commentsresult);
+              return res.render("/forumdata", {
+                name: req.user.name,
+                pid: pid,
+                postdata: postsresult.data,
+                comments: commentsresult
+              });
+            }
+          });
+      }
+    });
 };
 
 module.exports = { addforumpage, addforum };
--- backend/database/models/dislikes.js
+++ backend/database/models/dislikes.js
@@ -2,14 +2,12 @@ var mongoose = require("mongoose");
 
 var Schema = mongoose.Schema;
 
-var PostsSchema = new Schema({
+var DislikesSchema = new Schema({
   user_id: { type: Schema.Types.ObjectId, ref: "user", required: true },
   user_name: { type: String, ref: "user", required: true },
   user_email: { type: String, ref: "user", required: true },
-  data: { type: String, required: true },
-  likes: { type: Number, default: 0 },
-  dislikes: { type: Number, default: 0 },
-  comments: { type: Number, default: 0 }
+  post_id: { type: Schema.Types.ObjectId, ref: "posts", required: true },
+  data: { type: String, required: true }
 });
 
-module.exports = mongoose.model("posts", PostsSchema);
+module.exports = mongoose.model("comments", DislikesSchema);
